408cfa693c36758f145ea6e96c838da7a0185af2
Mzdhr <mmlail@gmail.com>
2016-08-04 13:46:49 +0300

    version0.1

--- data_grapper.py
+++ data_grapper.py
@@ -3,18 +3,18 @@ import urllib.request
 
 
 def fetch_info(title):
-    # Parsing url
-    API = 'http://www.omdbapi.com/?t=' + title.replace(' ', '+') + '&y=&plot=full&r=json'
-
-    # Graping data from the api
-    connection = urllib.request.urlopen(API)
-    output = connection.read().decode('utf-8')
-    connection.close()
-
-    # Creating dictionary using json object
-    api_data = json.loads(output)
-
     try:
+        # Parsing url
+        API = 'http://www.omdbapi.com/?t=' + title.replace(' ', '+') + '&y=&plot=full&r=json'
+
+        # Graping data from the api
+        connection = urllib.request.urlopen(API)
+        output = connection.read().decode('utf-8')
+        connection.close()
+
+        # Creating dictionary using json object
+        api_data = json.loads(output)
+
         # Gathering data
         box_info = [api_data['Title'], api_data['Runtime'], api_data['Type'], api_data['Year'], api_data['Genre'],
                     api_data['imdbRating'], api_data['imdbVotes'], api_data['Plot'], api_data['Director'], api_data['Actors'],
@@ -22,10 +22,11 @@ def fetch_info(title):
 
     except KeyError:
         box_info = False
+        print('Can\'t recognize movie title!')
 
     except urllib.request.URLError:
-        box_info = 'No Internet connection!'
+        box_info = False
+        print('No Internet connection!')
 
     return box_info
 
-print(fetch_info('ngasfs'))
